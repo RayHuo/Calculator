a12f12be6db508a9804c13527e024ee07a6d117a
RayHuo <rayhuo_81@hotmail.com>
2015-06-01 21:20:24 +0800

    初步布局

--- gen/com/ray/calculator/R.java
+++ gen/com/ray/calculator/R.java
@@ -22,9 +22,29 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int white_bg=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080013;
+        public static final int btn_0=0x7f080010;
+        public static final int btn_1=0x7f08000d;
+        public static final int btn_2=0x7f08000e;
+        public static final int btn_3=0x7f08000f;
+        public static final int btn_4=0x7f080009;
+        public static final int btn_5=0x7f08000a;
+        public static final int btn_6=0x7f08000b;
+        public static final int btn_7=0x7f080005;
+        public static final int btn_8=0x7f080006;
+        public static final int btn_9=0x7f080007;
+        public static final int btn_clear=0x7f080001;
+        public static final int btn_del=0x7f080002;
+        public static final int btn_div=0x7f080003;
+        public static final int btn_min=0x7f080008;
+        public static final int btn_mul=0x7f080004;
+        public static final int btn_plus=0x7f08000c;
+        public static final int btn_point=0x7f080011;
+        public static final int btn_result=0x7f080012;
+        public static final int input_screen=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
