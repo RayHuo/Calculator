16f4add3ed7bc85e5baef370f9da0fd42ce182e6
RayHuo <rayhuo_81@hotmail.com>
2015-06-02 02:36:18 +0800

    按动效果

--- src/com/ray/calculator/MainActivity.java
+++ src/com/ray/calculator/MainActivity.java
@@ -3,13 +3,17 @@ package com.ray.calculator;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		
 	}
 
 	@Override
@@ -17,6 +21,12 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
